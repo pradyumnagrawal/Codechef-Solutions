88758ee5eafa7477ada63e7a9c5fcbafaf36d948
Shyam Radadiya <67785558+shyam-ra18@users.noreply.github.com>
2020-10-11 22:29:24 +0530

    Update ATM HS08TEST.c
    This is a complete  ans. of atm question. no any errors

--- ATM HS08TEST.c
+++ ATM HS08TEST.c
@@ -1,22 +1,26 @@
-#include<stdio.h>
-int main()
-{
-unsigned int x; //amount withdrawn
-float y; //bank balance
-scanf("%d%f", &x,&y);
-if(((x>0)&&(x<=2000))&&((y>0)&&(y<=2000)))
-{
+#include <stdio.h>
 
-	if(x>y&&x%5==0)
-		printf("%0.2f", y);
-	else if(x%5==0)
-	{
-		y=(y-x)-0.5;
-		printf("%0.2f", y);
-	}
-	else
-		printf("%0.2f", y);
-}
-getchar();
-return 0;
+int main(void) {
+  int x;
+  float y;
+  scanf("%d",&x);
+  scanf("%f",&y);
+  
+  
+  
+   if(x%5==0&&(y-x-0.5)>=0)
+  {
+  y=y-0.50;
+  
+  
+  printf("%.2f",y-x );
+  }
+  else if(x%5!=0)
+  printf("%.2f",y);
+  else
+  
+  printf("%.2f",y);
+  
+  
+  return 0;
 }
